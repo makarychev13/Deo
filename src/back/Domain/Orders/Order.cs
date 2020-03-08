6d2370b9f9300e850639899afcd32f7d99b0f289
Mikhail <makar.tula@gmail.com>
2020-03-08 04:16:30 +0300

    Добавил доменную модель

---
+++ src/back/Domain/Orders/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain.Orders.ValueObjects;
+
+namespace Domain.Orders
+{
+    public class Order
+    {
+        public readonly string Title;
+        public readonly string Description;
+        public readonly Uri Link;
+        public readonly DateTime PublicationDate;
+
+        public readonly FreelanceBurse Burse;
+
+        public Order(string title, string description, Uri link, DateTime publicationDate, FreelanceBurse burse)
+        {
+            Title = title;
+            Description = description;
+            Link = link;
+            PublicationDate = publicationDate;
+            Burse = burse;
+        }
+    }
+}
---
+++ src/back/Domain/Orders/ValueObjects/FreelanceBurse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Orders.ValueObjects
+{
+    public class FreelanceBurse
+    {
+        public readonly Uri Link;
+        public readonly string Name;
+
+        public FreelanceBurse(Uri link, string name)
+        {
+            Link = link;
+            Name = name;
+        }
+    }
+}
