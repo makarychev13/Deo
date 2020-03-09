257e9eaa5c1b78d0d565de7d53ba2ef88705cd26
Mikhail <makar.tula@gmail.com>
2020-03-09 23:46:09 +0300

    Добавил запись сообщений в кафку

--- src/back/DomainServices/Orders/OrdersService.cs
+++ src/back/DomainServices/Orders/OrdersService.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Confluent.Kafka;
+using Domain.Orders;
 using Infrastructure.Orders.Rss.Reader;
 
 namespace DomainServices.Orders
 {
-    public sealed class OrdersService
+    public sealed class OrdersService : IDisposable
     {
         private readonly IOrdersReader _reader;
+        private readonly IProducer<Null, Order> _producer;
 
-        public OrdersService(IOrdersReader reader)
+        public OrdersService(IOrdersReader reader, IProducer<Null, Order> producer)
         {
             _reader = reader;
+            _producer = producer;
         }
 
         public async Task HandleOrdersAsync()
@@ -19,10 +24,19 @@ namespace DomainServices.Orders
             {
                 mutex.WaitOne(TimeSpan.FromMinutes(1));
                 var orders = await _reader.GetNewAsync();
-                //saveToKafka(orders)
+                foreach (var order in orders)
+                {
+                    await _producer.ProduceAsync("orders", new Message<Null, Order>() { Value = null });
+                }
+
                 _reader.UpdateOld(orders);
                 mutex.ReleaseMutex();
             }
-        } 
+        }
+
+        public void Dispose()
+        {
+            _producer?.Dispose();
+        }
     }
 }
