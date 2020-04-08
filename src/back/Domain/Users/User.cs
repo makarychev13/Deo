23ab7c82b5cf19fc53ed7c481af63750d775556f
Mikhail Makarychev <mmakarychev@ozon.ru>
2020-04-08 10:53:40 +0300

    Поправил доменную модель пользователя

--- src/back/Domain/Users/User.cs
+++ src/back/Domain/Users/User.cs
@@ -5,16 +5,14 @@ namespace Domain.Users
 {
     public sealed class User
     {
-        public readonly int Id;
         public readonly bool Active;
         public readonly Contact Contact;
         public readonly Subscriptions Subscriptions;
         public readonly string[] WhiteKeywords;
         public readonly string[] BlackKeywords;
 
-        public User(int id, bool active, Contact contact, Subscriptions subscriptions, string[] whiteKeywords, string[] blackKeywords)
+        public User(bool active, Contact contact, Subscriptions subscriptions, string[] whiteKeywords, string[] blackKeywords)
         {
-            Id = id;
             Active = active;
             Contact = contact;
             Subscriptions = subscriptions;
