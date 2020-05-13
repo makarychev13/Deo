7057aff9878bc04d87edc4e83661789218cc636c
Mikhail Makarychev <mmakarychev@ozon.ru>
2020-05-13 13:53:01 +0300

    [back] Исправил баг в EventBus

--- src/back/Common/Events/EventBus.cs
+++ src/back/Common/Events/EventBus.cs
@@ -6,7 +6,12 @@ namespace Common.Events
     internal sealed class EventBus<TK, TV> : IEventBus<TK, TV>
     {
         private readonly KafkaProducer<TK, TV> _producer;
-        
+
+        public EventBus(KafkaProducer<TK, TV> producer)
+        {
+            _producer = producer;
+        }
+
         public async Task PublishAsync(TK key, TV message)
         {
             await _producer.ProduceAsync(key, message);
